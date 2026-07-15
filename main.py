f08cecd22ad2148d58727549e42292cc8674c735
Leila Arowen Dumindin <laadumindin2022@plm.edu.ph>
2026-07-15 10:53:09 +0800

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+
+tasks_list = [{
+    "id" : "001",
+    "title" : "Finish Claude 101 Anthropic Course",
+    "done" : False
+},
+{
+    "id" : "002",
+    "title" : "Finish Backend Assignment 1: Building First CRUD API",
+    "done" : False
+},
+{
+    "id" : "003",
+    "title" : "Finish AI Fluency Assignment 1: AI Workflow Audit and Tool Setup ",
+    "done" : False
+}
+]
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -12,3 +28,19 @@ async def root():
 @app.get("/health")
 def health():
     return {"status": "Ok"}
+
+@app.get("/tasks")
+def return_task_list():
+    return tasks_list
+
+@app.get("/tasks/{id}")
+def return_task(id: str):
+    for task in tasks_list:
+        if task["id"] == id:
+            return task
+        
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f"Task with {id} not found"
+    )
+
