26f9bd7162edd366ff442d709ca44b1d6df58e36
Leila Arowen Dumindin <laadumindin2022@plm.edu.ph>
2026-07-15 10:25:58 +0800

    Stage 1: Root and Health Endpoints

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def home():
+    return { "name": "Task API", 
+            "version": "1.0", 
+            "endpoints": ["/tasks"] }
+
+@app.get("/health")
+def health():
+    return {"status": "Ok"}
